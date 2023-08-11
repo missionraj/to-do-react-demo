7d6e605296c66af37ca73cdae275a4be3deec9f9
raj rana <raj.rana507@gmail.com>
2023-08-11 19:22:24 +0530

    feat: "first commit"

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { useState, useEffect } from "react";
 function App() {
+  const [input, setInput] = useState("");
+  const [lists, setLists] = useState([]);
+
+  useEffect(() => {
+    const lists = JSON.parse(localStorage.getItem("todolists"));
+    if (lists) {
+      setLists(lists);
+    }
+  }, []);
+
+  const inputChangeHandler = (e) => {
+    setInput(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLists([...lists, input]);
+    localStorage.setItem("todolists", JSON.stringify(lists));
+    setInput("");
+  };
+
+  const handleDeleteTasks = (index) => {
+    const updatedList = [...lists.slice(0, index), ...lists.slice(index + 1)];
+    setLists(updatedList);
+    localStorage.setItem("todolists", JSON.stringify(updatedList));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={input}
+          placeholder="add to do"
+          onChange={inputChangeHandler}
+        />
+        <button type="submit"> submit </button>
+      </form>
+      <ul>
+        {lists.map((el, i) => {
+          return (
+            <li key={i}>
+              {el} <button onClick={() => handleDeleteTasks(i)}> Delete</button>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
